2bab46642c04f53fcd663a2cb4fcd858530790b7
tom <tom.meyer@uni-rostock.de>
2018-02-08 11:12:03 +0100

    extended testscript

--- testscript.sh
+++ testscript.sh
@@ -9,8 +9,11 @@ export CXXFLAGS=-std=c++11
 ./configure
 make -j4
 
+./src/lola --json="test-master-t1-b0.json" --check=full --threads=1 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-master-t1-b100.json" --check=full --threads=1 --bucketing=100 ./tests/testfiles/phils1000.lola
-./src/lola --json="test-master-t5-b100.json" --check=full --threads=5 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-master-t2-b100.json" --check=full --threads=2 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-master-t3-b100.json" --check=full --threads=4 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-master-t4-b100.json" --check=full --threads=4 --bucketing=100 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-master-t20-b100.json" --check=full --threads=20 --bucketing=100 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-master-t60-b100.json" --check=full --threads=60 --bucketing=100 ./tests/testfiles/phils1000.lola
 
@@ -23,8 +26,11 @@ export CXXFLAGS=-std=c++11
 ./configure
 make -j4
 
-./src/lola --json="test-atomics-t1-b100.json" --check=full --threads=1 --bucketing=100 ./tests/testfiles/phils1000.lola
-./src/lola --json="test-atomics-t5-b100.json" --check=full --threads=5 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-atomics-t1-b100.json" --check=full --threads=1 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-atomics-t1-b0.json" --check=full --threads=1 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-atomics-t2-b100.json" --check=full --threads=2 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-atomics-t3-b100.json" --check=full --threads=3 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-atomics-t4-b100.json" --check=full --threads=4 --bucketing=100 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-atomics-t20-b100.json" --check=full --threads=20 --bucketing=100 ./tests/testfiles/phils1000.lola
 # ./src/lola --json="test-atomics-t60-b100.json" --check=full --threads=60 --bucketing=100 ./tests/testfiles/phils1000.lola
 
@@ -37,13 +43,18 @@ export CXXFLAGS=-std=c++11
 ./configure
 make -j4
 
+./src/lola --json="test-maraPThread-t1-b0.json" --check=full --threads=1 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-maraPThread-t1-b100.json" --check=full --threads=1 --bucketing=100 ./tests/testfiles/phils1000.lola
-./src/lola --json="test-maraPThread-t5-b100.json" --check=full --threads=5 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-maraPThread-t2-b100.json" --check=full --threads=2 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-maraPThread-t3-b100.json" --check=full --threads=3 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-maraPThread-t4-b100.json" --check=full --threads=4 --bucketing=100 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-maraPThread-t20-b100.json" --check=full --threads=20 --bucketing=100 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-maraPThread-t60-b100.json" --check=full --threads=60 --bucketing=100 ./tests/testfiles/phils1000.lola
 
 ./src/lola --json="test-maraPThread-t1-b1000.json" --check=full --threads=1 --bucketing=1000 ./tests/testfiles/phils1000.lola
-./src/lola --json="test-maraPThread-t5-b1000.json" --check=full --threads=5 --bucketing=1000 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-maraPThread-t2-b1000.json" --check=full --threads=2 --bucketing=1000 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-maraPThread-t3-b1000.json" --check=full --threads=3 --bucketing=1000 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-maraPThread-t4-b1000.json" --check=full --threads=4 --bucketing=1000 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-maraPThread-t20-b1000.json" --check=full --threads=20 --bucketing=1000 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-maraPThread-t60-b1000.json" --check=full --threads=60 --bucketing=1000 ./tests/testfiles/phils1000.lola
 
@@ -56,12 +67,20 @@ export CXXFLAGS=-std=c++11
 ./configure
 make -j4
 
+./src/lola --json="test-maraIntegration-t1-b0.json" --check=full --threads=1 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-maraIntegration-t1-b100.json" --check=full --threads=1 --bucketing=100 ./tests/testfiles/phils1000.lola
-./src/lola --json="test-maraIntegration-t5-b100.json" --check=full --threads=5 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-maraIntegration-t2-b100.json" --check=full --threads=2 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-maraIntegration-t3-b100.json" --check=full --threads=3 --bucketing=100 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-maraIntegration-t4-b100.json" --check=full --threads=4 --bucketing=100 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-maraIntegration-t20-b100.json" --check=full --threads=20 --bucketing=100 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-maraIntegration-t60-b100.json" --check=full --threads=60 --bucketing=100 ./tests/testfiles/phils1000.lola
 
 ./src/lola --json="test-maraIntegration-t1-b1000.json" --check=full --threads=1 --bucketing=1000 ./tests/testfiles/phils1000.lola
-./src/lola --json="test-maraIntegration-t5-b1000.json" --check=full --threads=5 --bucketing=1000 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-maraIntegration-t2-b1000.json" --check=full --threads=2 --bucketing=1000 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-maraIntegration-t3-b1000.json" --check=full --threads=3 --bucketing=1000 ./tests/testfiles/phils1000.lola
+./src/lola --json="test-maraIntegration-t4-b1000.json" --check=full --threads=4 --bucketing=1000 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-maraIntegration-t20-b1000.json" --check=full --threads=20 --bucketing=1000 ./tests/testfiles/phils1000.lola
 ./src/lola --json="test-maraIntegration-t60-b1000.json" --check=full --threads=60 --bucketing=1000 ./tests/testfiles/phils1000.lola
+
+#print results to file
+grep 'runtime' test*.json > times.txt
