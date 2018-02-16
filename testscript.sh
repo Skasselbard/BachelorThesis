ed29516d5ee7cedc0f1eda1153365516b269c4ab
Skasselbard <latz1@gmx.net>
2018-02-16 18:42:55 +0100

    quotes for cflags

--- testscript.sh
+++ testscript.sh
@@ -3,7 +3,7 @@
 exec > test.out                                                                      
 exec 2>&1
 
-CXXFLAGS=-std=c++11
+CXXFLAGS='-std=c++11'
 #testiteration
 for i in 1 2 3 4 5 6 7 8 9 10
 do
